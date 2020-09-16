4dea95e7694a422152bad9d29f0db520b93efc25
johnnfujita <johnnie.fujita@gmail.com>
2020-09-16 15:11:37 -0300

    fixing some issues

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout.js'
 const about = () => {
     return (
         <Layout>
-            <h1>aadfasdfafadf</h1>
+            <h1>Em desenvolvimento...</h1>
         </Layout>
     )
 }
